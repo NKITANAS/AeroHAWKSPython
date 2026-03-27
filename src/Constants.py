a3cebcc7cc8208e912863326dfb3f6d84e35a7d7
Nikita Nasiedkin <nkitanas@proton.me>
2026-03-27 00:10:51 -0400

    small change to make code usable on pi

--- src/Constants.py
+++ src/Constants.py
@@ -1,10 +1,5 @@
-import gpiod
-from mpu6050 import mpu6050
-
 # Test Mode #
 TEST_MODE: bool = True
-
-gpiochip: gpiod.Chip = gpiod.Chip('/dev/gpiochip0')
 
 # Pico
 PICO_SERIAL_PORT: str = '/dev/serial0'
@@ -14,46 +9,5 @@ PICO_BAUD_RATE: int = 115200
 LOG_FILE_PATH: str = 'aerohawks_data.log'
 
 # Math Constants #
-
 GRAVITY: float = 9.80665          # m/s^2
 GAS_CONSTANT: float = 8.314462618 # J/(mol*K)
-
-# Pin/Line definitions #
-### NOTE: DONT USE 8 AND 9
-
-# Linear Actuators - numbers are subject to change
-LINEAR_ACTUATOR_L_PIN_1: int = 6
-LINEAR_ACTUATOR_L_PIN_2: int = 13
-LINEAR_ACTUATOR_R_PIN_1: int = 19
-LINEAR_ACTUATOR_R_PIN_2: int = 26
-
-# Stepper Motor - numbers are subject to change
-STEPPER_MOTOR_DIR_PIN:   int = 23
-STEPPER_MOTOR_STEP_PIN:  int = 24
-
-# Stepper Motor Windows positions #
-DEGREE_TO_STEP: float = 200 / 360 # 200 steps per revolution, 360 degrees per revolution
-
-WINDOW1_STEPS: float  = 0   * DEGREE_TO_STEP  
-WINDOW2_STEPS: float  = 90  * DEGREE_TO_STEP  
-WINDOW3_STEPS: float  = 0   * DEGREE_TO_STEP  
-WINDOW4_STEPS: float  = 90  * DEGREE_TO_STEP  
-
-default_settings: gpiod.LineSettings = gpiod.line_settings.LineSettings(
-    direction=gpiod.line.Direction.OUTPUT
-    )
-input_settings: gpiod.LineSettings = gpiod.line_settings.LineSettings(
-    direction=gpiod.line.Direction.INPUT
-    )
-
-request: gpiod.LineRequest = gpiochip.request_lines(
-    consumer="payload",
-    config={
-        LINEAR_ACTUATOR_L_PIN_1: default_settings,
-        LINEAR_ACTUATOR_L_PIN_2: default_settings,
-        LINEAR_ACTUATOR_R_PIN_1: default_settings,
-        LINEAR_ACTUATOR_R_PIN_2: default_settings,
-        STEPPER_MOTOR_DIR_PIN:   default_settings,
-        STEPPER_MOTOR_STEP_PIN:  default_settings,
-    },
-)
